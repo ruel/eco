2b2cb5b154d32197e436778ccbae0c81d2da1bc0
Ruel Pagayon <ruel@ruel.me>
2015-09-06 03:07:44 +0800

    Added details on user controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   getUser: function(req, res) {
-    User.findOne({ id: req.params.user }, function(err, user) {
+    User.findOne({ id: req.params.user }).populate('projects').exec(function(err, user) {
       if (err) {
         sails.log.error(err);
         return res.serverError();
@@ -9,26 +9,51 @@ module.exports = {
 
       if (!user) return res.notFound();
 
-      Fund.sum(user.id, 'user', function(err, total) {
-        if (err) {
-          sails.log.error(err);
-          return res.serverError();
+      async.auto({
+
+        total: function(autoCallback) {
+          Find.sum(user.id, 'user', autoCallback);
+        },
+
+        level: function(autoCallback) {
+          Level.find({ '<': { min: total } }, function(err, levels) {
+            if (err) {
+              sails.log.error(err);
+              return res.serverError();
+            }
+
+            var level = _.max(levels, function(l) { return l.min });
+            autoCallback(null, level);
+          });
+        },
+
+        supported: function(autoCallback) {
+          
+          var supported = [];
+          async.each(user.funds, function(fund, eachCallback) {
+
+            Project.list({ id: fund.project }, { limit: 1, page: 0 }, 'createdAt DESC', function(err, projects) {
+              if (err) return eachCallback(err);
+              if (projects.length < 1) return eachCallback('not_found');
+
+              supported.push(projects[0]);
+              eachCallback();
+            });
+
+          }, function(err) {
+            if (err) return autoCallback(err);
+            autoCallback(null, supported);
+          });
+
         }
 
-        Level.find({ '<': { min: total } }, function(err, levels) {
-          if (err) {
-            sails.log.error(err);
-            return res.serverError();
-          }
+      }, function(err, result) {
+        user.level = result.level;
+        user.total = result.total;
+        user.supported = result.supported;
 
-          var level = _.max(levels, function(l) { return l.min });
-
-          user.level = level;
-          user.total = total;
-
-          res.view('user', {
-            user: user
-          });
+        res.view('user', {
+          user: user
         });
       });
     });
