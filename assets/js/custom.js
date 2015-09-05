e4b7cda8e5fb421dba3333e1337b42d846a3e432
Rachel de Villa <rldevilla@voyagerinnovation.com>
2015-09-05 23:07:42 +0800

    Added project add page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var path = window.location.pathname;
   
-  if(path.indexOf('/auth') > -1){
+  if(path.indexOf('/auth') > -1 || path.indexOf('/projects') > -1){
     console.log('here')
     $('body').css('background-color', '#f7fafa');
   }
