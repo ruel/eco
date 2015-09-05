99dc8124514f432234f9b837a35f25a598149840
Ruel Pagayon <ruel@ruel.me>
2015-09-05 19:22:43 +0800

    Add login and logout to route

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,11 @@ module.exports.routes = {
   'get /auth/register': 'Auth.getRegister',
   'post /auth/register': 'Auth.postRegister',
 
+  'get /auth/login': 'Auth.getLogin',
+  'post /auth/login': 'Auth.postLogin',
+  
+  'get /auth/logout': 'Auth.logout',
+
   'get /auth/facebook': 'Auth.facebookLogin',
   'get /auth/facebook/callback': 'Auth.facebookCallback',
 };
