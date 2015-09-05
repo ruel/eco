10e8e0d998bb1a90236606e0fe44eceb9e8eb56f
Ruel Pagayon <ruel@ruel.me>
2015-09-05 18:53:00 +0800

    Add test facebook credentials

--- config/facebook.js
+++ config/facebook.js
@@ -1,5 +1,5 @@
 module.exports.facebook = {
-  clientID: '',
-  clientSecret: '',
+  clientID: '1492950464360356',
+  clientSecret: '1f7350372b906f013a1b7f1f115290e8',
   callbackURL: 'http://localhost:1338/auth/facebook/callback'
 };
