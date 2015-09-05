360967759828c06250956c46756c59f07bb8890d
Ruel Pagayon <ruel@ruel.me>
2015-09-05 19:04:55 +0800

    Add isValidationError

--- api/services/Utils.js
+++ api/services/Utils.js
@@ -4,4 +4,27 @@ module.exports = {
   MD5: function(string) {
     return crypto.createHash('md5').update(string).digest('hex');
   },
+
+  isValidationError: function(err) {
+    if (_.isPlainObject(err)) {
+      var keys = Object.keys(err);
+      if (keys.length) {
+        var failedValidation = err[keys[0]];
+        if (_.isArray(failedValidation) && failedValidation.length &&
+          _.isPlainObject(failedValidation[0]) && failedValidation[0]['rule']) {
+          return true;
+        }
+      }
+    }
+
+    if ( _.isString(err) && err.match(/duplicate key value violates unique constraint/g) ) {
+      return true;
+    }
+   
+    if ( _.isString(err) && err.match(/^Bad request/ig)) {
+      return true;
+    }
+
+    return false;
+  }
 };
