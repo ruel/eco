3da8d01da89d8e6b90d58ea88c4925f310a53389
Ruel Pagayon <ruel@ruel.me>
2015-09-05 22:15:08 +0800

    Paypal creds, and yes, I intentionally pushed this

---
+++ config/paypal.js
@@ -0,0 +1,5 @@
+module.exports.paypal = {
+  mode: 'sandbox',
+  client_id: 'AbyMSiLBPM6gbGcmOLlYTiTXn7-Vrt6dgqR49PoH0eXww8PB5levrru5O2LVvg062TZ3IWOnMso8sTSK',
+  client_secret: 'ED1Ckr4OfBQ1f2pmJ0NIMmiQ8dh8kT3_knVEA3wRlsrKEYke6t-jXB-z8w7dwxApFGVhPgmlxn3FDZLo'
+};
