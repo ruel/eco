2c3a70f118b7d13d167a10307223180cffe41a34
Ruel Pagayon <ruel@ruel.me>
2015-09-05 19:26:18 +0800

    Adjust policy config

--- config/policies.js
+++ config/policies.js
@@ -1,5 +1,13 @@
 module.exports.policies = {
 
   '*': true,
+  
+  AuthController: {
+    facebookLogin: 'sessionCheck',
+    getLogin: 'sessionCheck',
+    postLogin: 'sessionCheck',
+    getRegister: 'sessionCheck',
+    postRegister: 'sessionCheck',
+  },
 
 };
