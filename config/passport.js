cfc3fe557854f32f942511f8dabd2eb20e55a0f5
Ruel Pagayon <ruel@ruel.me>
2015-09-06 01:52:25 +0800

    Added passport config

---
+++ config/passport.js
@@ -0,0 +1,10 @@
+var passport = require('passport');
+
+module.exports = {
+  http: {
+    customMiddleware: function(app) {
+      app.use(passport.initialize());
+      app.use(passport.session());
+    }
+  }
+}
