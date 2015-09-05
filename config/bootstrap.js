9ec3aa9633232f105be32a86fa3d546de2eba1db
Ruel Pagayon <ruel@ruel.me>
2015-09-05 18:24:19 +0800

    Addepassport to bootstrap

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,17 +1,51 @@
-/**
- * Bootstrap
- * (sails.config.bootstrap)
- *
- * An asynchronous bootstrap function that runs before your Sails app gets lifted.
- * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
- *
- * For more information on bootstrapping your app, check out:
- * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
- */
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports.bootstrap = function(cb) {
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findOne({ id: id }, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use(new FacebookStrategy({
+    clientID: sails.config.facebook.clientID,
+    clientSecret: sails.config.facebook.clientSecret,
+    callbackURL: sails.config.facebook.callbackURL,
+    enableProof: false
+  }, function(accessToken, refreshToken, profile, done) {
+    User.findOne({ username: profile.id }, function(err, user) {
+      if (err) return done(err);
+
+      var userObj = {
+        first_name: profile['_json'].first_name,
+        last_name: profile['_json'].last_name,
+        email: profile['_json'].email,
+        username: profile.id
+      };
+
+      if (!user) return done(null, false, userObj);
+      done(null, user);
+    });
+  }));
+
+  passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+  }, function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) return done(err);
+      if (!user || Utils.MD5(password) === user.password) return return done(null, false, { message: 'Invalid username or password' });
+
+      done(null, user, { message: 'Logged-in successfully' });
+    });
+  }));
+
   cb();
 };
