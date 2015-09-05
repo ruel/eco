2e6a0567aa91d4a2a4d2f76525e41a75319f7002
Ruel Pagayon <ruel@ruel.me>
2015-09-05 21:03:47 +0800

    Add Fund.sum

--- api/models/Fund.js
+++ api/models/Fund.js
@@ -18,6 +18,19 @@ module.exports = {
       required: true
     },
 
+  },
+
+  sum: function(project, callback) {
+    Fund.find({ project: project }, function(err, funds) {
+      if (err) return callback(err);
+
+      var fundList = _.map(funds, function(f) { return f.amount });
+      var total = _.reduce(fundList, function(amount, total) {
+        return amount + total;
+      }, 0);
+
+      callback(null, total);
+    });
   }
 
 };
