d32133e7cdce64827c41341b3f5bd013353e2a2d
Ruel Pagayon <ruel@ruel.me>
2015-09-05 18:46:53 +0800

    Add passport init

--- config/http.js
+++ config/http.js
@@ -1,14 +1,3 @@
-/**
- * HTTP Server Settings
- * (sails.config.http)
- *
- * Configuration for the underlying HTTP server in Sails.
- * Only applies to HTTP requests (not WebSockets)
- *
- * For more information on configuration, check out:
- * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.http.html
- */
-
 module.exports.http = {
 
   /****************************************************************************
@@ -21,7 +10,10 @@ module.exports.http = {
   *                                                                           *
   ****************************************************************************/
 
-  // middleware: {
+  middleware: {
+
+    passportInit: require('passport').initialize(),
+    passportSession: require('passport').session(),
 
   /***************************************************************************
   *                                                                          *
@@ -71,7 +63,7 @@ module.exports.http = {
 
     // bodyParser: require('skipper')
 
-  // },
+  },
 
   /***************************************************************************
   *                                                                          *
